b76d14b30882591b18c0994050a80059c64ae2fc
Semper0n <tyutchevivan@rambler.ru>
2022-10-30 19:34:33 +0300

    Add body for functions
    Добавил тела для функций summation и multiplication_on_number, а также добавил функцию substraction

--- matrix_calc.py
+++ matrix_calc.py
@@ -1,28 +1,48 @@
+# Функция обрабатывающая операцию сложения двух матриц
 def summation(matrix_a, matrix_b):
-    pass
+    for i in range(0, len(matrix_a)):
+        for j in range(0, len(matrix_a[i])):
+            matrix_a[i][j] += matrix_b[i][j]
+    return matrix_a
 
 
-def multiplication_on_number(number, matirx):
-    pass
+# Функция обрабатывающая операцию вычитание двух матриц
+def subtraction(matrix_a, matrix_b):
+    for i in range(0, len(matrix_a)):
+        for j in range(0, len(matrix_a[i])):
+            matrix_a[i][j] -= matrix_b[i][j]
+    return matrix_a
 
 
+# Функция обрабатывающая операцию умножения матрицы на число
+def multiplication_on_number(number, matrix):
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[i])):
+            matrix[i][j] *= number
+    return matrix
+
+
+# Функция обрабатывающая операцию умножения двух матриц
 def multiplication(matrix_a, matrix_b):
     pass
 
 
+# Словарь операций
 task_dict = {"Сложение": summation,
+             "Вычитание": subtraction,
              "Умножение на число": multiplication_on_number,
              "Умножение матриц": multiplication}
 
 
+# Функция обрабатывающая ввод матриц и выбор операции
 def self_input():
     task_key = input("Введите интересующую вас операцию: ")
-
+    pass
 
 
 def main():
     print("\n\t\t\tДобро пожаловать в Matrix_Calc!!!\n")
-    self_input()
+    print(multiplication_on_number(5, [[3, 2, 1], [1, 2, 3]]))
 
 
 if __name__ == "__main__":
