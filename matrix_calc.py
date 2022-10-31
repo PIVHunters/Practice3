9711deb9c2c93c196c4049de6c77929b88609939
Semper0n <tyutchevivan@rambler.ru>
2022-10-31 23:03:06 +0300

    Big changes
    Добавил тело для функции multiplication(), transposition(), вложил функцию validator в тело функции self_input(), изменил функцию main()

--- matrix_calc.py
+++ matrix_calc.py
@@ -15,7 +15,7 @@ def subtraction(matrix_a, matrix_b):
 
 
 # Функция обрабатывающая операцию умножения матрицы на число
-def multiplication_on_number(number, matrix):
+def multiplication_on_number(matrix, number):
     for i in range(0, len(matrix)):
         for j in range(0, len(matrix[i])):
             matrix[i][j] *= number
@@ -24,12 +24,18 @@ def multiplication_on_number(number, matrix):
 
 # Функция обрабатывающая операцию умножения двух матриц
 def multiplication(matrix_a, matrix_b):
-    pass
+    matrix_result = [[0 for row in range(len(matrix_a))] for col in range(len(matrix_b[0]))]
+    for i in range(len(matrix_a)):
+        for j in range(len(matrix_b[0])):
+            for k in range(len(matrix_b)):
+                matrix_result[i][j] += matrix_a[i][k] * matrix_b[k][j]
+    return matrix_result
 
 
 # Функция обрабатывающая операцию транспонирования
 def transposition(matrix):
-    pass
+    matrix_result = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+    return matrix_result
 
 
 # Функция обрабатывающая операцию умножения двух матриц
@@ -53,45 +59,40 @@ def self_input():
     print("Введите задачу - ")
     task = input("Например, Сложение: ")  # По задаче определяем сколько параметров запросим ввести
 
-    if task in binary_dict.keys():
-        try:  # try ... exept - Отлавливает ввод буквенных значений
-            first_arg = input("Матрица в формате [[3,2,1][1,1,1][1,2,3]]: ")
-            if first_arg in ["", " "]:  # Проверка на пустой ввод
-                print("\n\t\t\tВы ошиблись в параметрах!\n\t\t\t   Выполните ввод заново")
-                return self_input()
-            if first_arg.isnumeric():  # Проверка на введение числа
-                print("\n\t\t\tВы ошиблись в параметрах!\n\t\t\t  Выполните ввод заново")
-                return self_input()
-            first_arg = first_arg.replace('[', '').split(']')
-            first_arg = [value.split(',') for value in first_arg if value != '']
-            first_arg = [[float(cell) for cell in row] for row in first_arg]
+    if task in binary_dict.keys() or task in unary_dict.keys():
+        try:
+            rows = int(input("Введите кол-во строк в матрице: "))
+            coloumns = int(input("Введите кол-во столбцов в матрице: "))
+            first_arg = []
+            for i in range(rows):
+                first_arg.append([])
+            for i in range(rows):
+                for j in range(coloumns):
+                    first_arg[i].append(int(input(f"Введите элемент A{i + 1}{j + 1} = ")))
 
-            second_arg = input("Матрица или число: ")
-            if second_arg in ["", " "]:  # Проверка на пустой ввод
-                print("\n\t\t\tВы ошиблись в параметрах!\n\t\t\t  Выполните ввод заново")
-                return self_input()
-            if second_arg.isnumeric():
-                valid_data = validator(task, first_arg, float(second_arg))
-            else:
-                second_arg = second_arg.replace('[', '').split(']')
-                second_arg = [value.split(',') for value in second_arg if value != '']
-                second_arg = [[float(cell) for cell in row] for row in second_arg]
-                valid_data = validator(task, first_arg, second_arg)
-            return valid_data
-        except ValueError:
-            print("\n\t\t\tВы ошиблись в параметрах!\n\t\t\t  Выполните ввод заново")
-            return self_input()
-
-    if task in unary_dict.keys():
-        try:  # try ... exept - Отлавливает ввод буквенных значений
-            arg = input("Матрица в формате [[3,2,1][1,1,1][1,2,3]]: ")
-            if arg in ["", " "]:  # Проверка на пустой ввод
-                print("\n\t\t\tВы ошиблись в параметрах!\n\t\t\t  Выполните ввод заново")
-                return self_input()
-            arg = arg.replace('[', '').split(']')
-            arg = [value.split(',') for value in arg if value != '']
-            arg = [[float(cell) for cell in row] for row in arg]
-            return arg
+            match task:
+                case "Сложение" | "Вычитание":
+                    second_arg = []
+                    for i in range(rows):
+                        second_arg.append([])
+                    for i in range(rows):
+                        for j in range(coloumns):
+                            second_arg[i].append(int(input(f"Введите элемент B{i + 1}{j + 1} = ")))
+                    return task, first_arg, second_arg
+                case "Умножение на число":
+                    number = int(input("Введите число на которое нужно умножить матрицу: "))
+                    return task, first_arg, number
+                case "Перемножение":
+                    coloumns = int(input("Введите кол-во столбцов во второй матрице: "))
+                    second_arg = []
+                    for i in range(rows):
+                        second_arg.append([])
+                    for i in range(rows):
+                        for j in range(coloumns):
+                            second_arg[i].append(int(input(f"Введите элемент B{i + 1}{j + 1} = ")))
+                    return task, first_arg, second_arg
+                case "Транспонирование" | "Нахождение обратной":
+                    return task, first_arg
         except ValueError:
             print("\n\t\t\tВы ошиблись в параметрах!\n\t\t\t  Выполните ввод заново")
             return self_input()
@@ -100,36 +101,14 @@ def self_input():
     return self_input()
 
 
-# Функция порверяющая правильность аргументов для заданной задачи
-def validator(task, *args, **kwargs):
-    if len(args) != 1:
-        if task == "Умножение на число" and type(args[1]) == float:
-            return task, args[0], args[1]
-        if task == "Перемножение" and type(args[1]) == list and len(args[0][0]) == len(args[1]):
-            return task, args[0], args[1]
-        if task == "Сложение" and type(args[1]) == list:
-            return task, args[0], args[1]
-        if task == "Вычитание" and type(args[1]) == list:
-            return task, args[0], args[1]
-    else:
-        if task == "Транспонирование" and type(args[0]) == list:
-            return task, args[0], args[1]
-        if task == "Нахождение обратной матрицы" and type(args[0]) == list:
-            return task, args[0], args[1]
-
-    print("\n\t\t\tВы ошиблись в параметрах!\n\t\t\t  Выполните ввод заново")
-    return self_input()
-
-
 def main():
     print("\n\t\t\tДобро пожаловать в Matrix_Calc!!!\n")
-    valid_data = self_input()
-    if valid_data[0] in binary_dict.keys():
-        answer = binary_dict[valid_data[0]](valid_data[1], valid_data[2])
-        print(f"Ответ ==> {answer}")
-    if valid_data[0] in unary_dict.keys():
-        answer = unary_dict[valid_data[0]](valid_data[1])
-        print(f"Ответ ==> {answer}")
+    data = self_input()
+    if data[0] in binary_dict.keys():
+        print(f"ANSWER ==> {binary_dict[data[0]](data[1], data[2])}")
+        return 0
+    print(f"ANSWER ==> {unary_dict[data[0]](data[1])}")
+    return 0
 
 
 if __name__ == "__main__":
@@ -167,4 +146,82 @@ if __name__ == "__main__":
         second_arg = [value.split(',') for value in second_arg if value != '']
         second_arg = [[float(cell) for cell in row] for row in second_arg]
         return task, first_arg, second_arg
+
+    # valid_data = self_input()
+    # if valid_data[0] in binary_dict.keys():
+    #     answer = binary_dict[valid_data[0]](valid_data[1], valid_data[2])
+    #     print(f"Ответ ==> {answer}")
+    # if valid_data[0] in unary_dict.keys():
+    #     answer = unary_dict[valid_data[0]](valid_data[1])
+    #     print(f"Ответ ==> {answer}")
+
+
+
+        if task in binary_dict.keys(): 
+        try: # try ... exept - Отлавливает ввод буквенных значений
+            first_arg = input("Матрица в формате [[3,2,1][1,1,1][1,2,3]]: ")
+            if first_arg in ["", " "]: # Проверка на пустой ввод
+                print("\n\t\t\tВы ошиблись в параметрах!\n\t\t\t   Выполните ввод заново")
+                return self_input()
+            if first_arg.isnumeric(): # Проверка на введение числа
+                print("\n\t\t\tВы ошиблись в параметрах!\n\t\t\t  Выполните ввод заново")
+                return self_input()
+            first_arg = first_arg.replace('[','').split(']')
+            first_arg = [value.split(',') for value in first_arg if value != '']
+            first_arg = [[float(cell) for cell in row] for row in first_arg]
+
+            second_arg = input("Матрица или число: ")
+            if second_arg in ["", " "]: # Проверка на пустой ввод
+                print("\n\t\t\tВы ошиблись в параметрах!\n\t\t\t  Выполните ввод заново")
+                return self_input()
+            if second_arg.isnumeric():
+                valid_data = validator(task, first_arg, float(second_arg))
+            else:
+                second_arg = second_arg.replace('[','').split(']')
+                second_arg = [value.split(',') for value in second_arg if value != '']
+                second_arg = [[float(cell) for cell in row] for row in second_arg]
+                valid_data = validator(task, first_arg, second_arg)
+            return valid_data
+        except ValueError:
+            print("\n\t\t\tВы ошиблись в параметрах!\n\t\t\t  Выполните ввод заново")
+            return self_input()
+
+    if task in unary_dict.keys():
+        try: # try ... exept - Отлавливает ввод буквенных значений
+            arg = input("Матрица в формате [[3,2,1][1,1,1][1,2,3]]: ")
+            if arg in ["", " "]: # Проверка на пустой ввод
+                print("\n\t\t\tВы ошиблись в параметрах!\n\t\t\t  Выполните ввод заново")
+                return self_input()
+            arg = arg.replace('[','').split(']')
+            arg = [value.split(',') for value in arg if value != '']
+            arg = [[float(cell) for cell in row] for row in arg]
+            return arg
+        except ValueError:
+            print("\n\t\t\tВы ошиблись в параметрах!\n\t\t\t  Выполните ввод заново")
+            return self_input()
+
+    print("\n\t\t\tДанная функция еще не реализована\n\t\t\t\tПопробуйте еще раз!")
+    return self_input()
+
+
+
+    # Функция порверяющая правильность аргументов для заданной задачи
+def validator(task, *args, **kwargs):
+    if len(args) != 1:
+        if task == "Умножение на число" and type(args[1]) == float:
+            return task, args[0], args[1]
+        if task == "Перемножение" and type(args[1]) == list and len(args[0][0]) == len(args[1]):
+            return task, args[0], args[1]
+        if task == "Сложение" and type(args[1]) == list:
+            return task, args[0], args[1]
+        if task == "Вычитание" and type(args[1]) == list:
+            return task, args[0], args[1]
+    else:
+        if task == "Транспонирование" and type(args[0]) == list:
+            return task, args[0], args[1]
+        if task == "Нахождение обратной матрицы" and type(args[0]) == list:
+            return task, args[0], args[1]
+
+    print("\n\t\t\tВы ошиблись в параметрах!\n\t\t\t  Выполните ввод заново")
+    return self_input()
 '''
